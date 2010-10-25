47fce963356dda6c98e70825b58b46b653f2a36b
carlstaaf <carlstaaf@a23a6042-b2fc-11de-aad8-5f085193a258>
2010-10-25 18:28:20 +0000

    Refactored splash screen to load a single JPEG, with all text baked in.  This allows non-programmers to easily update the contents.  Added example splash screen image.

--- src/edu/unm/casaa/main/SplashWindow.java
+++ src/edu/unm/casaa/main/SplashWindow.java
@@ -2,11 +2,7 @@ package edu.unm.casaa.main;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Toolkit;
 
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -21,27 +17,15 @@ public class SplashWindow extends JWindow {
 
         content.setBackground( Color.white );
 
-        Dimension   screen  = Toolkit.getDefaultToolkit().getScreenSize();
-        int         width   = Math.min( screen.width / 2, 600 );
-        int         height  = Math.min( screen.height / 2, 250 );
+        int         width   = 800;
+        int         height  = 600;
 
         setSize( width, height );
         setLocationRelativeTo( null ); // Center on screen.
 
         // Build the splash screen.
-        // TODO - Get contents from UNM.
-        JLabel image    = new JLabel( new ImageIcon( "images/UNM_Color.jpg" ) );
-        JLabel title    = new JLabel( "CACTI | The CASAA Application for Coding Treatment Interactions", JLabel.CENTER );
-        JLabel version  = new JLabel( "v" + Version.versionString(), JLabel.CENTER );
+        JLabel image    = new JLabel( new ImageIcon( "images/Splash.jpg" ) );
 
-        title.setFont( new Font( "Sans-Serif", Font.PLAIN, 16 ) );
-        version.setFont( new Font( "Sans-Serif", Font.PLAIN, 14 ) );
-        content.add( title, BorderLayout.NORTH );
         content.add( image, BorderLayout.CENTER );
-        content.add( version, BorderLayout.SOUTH );
-
-        Color oraRed = new Color( 156, 20, 20, 255 );
-
-        content.setBorder( BorderFactory.createLineBorder( oraRed, 5 ) );
     }
 }
